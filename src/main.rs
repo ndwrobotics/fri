58513a9c65bdebee2232d9e56deed3e041059d81
ndwrobotics <22948910+ndwrobotics@users.noreply.github.com>
2025-06-25 17:40:49 -0700

    Add polynomial merkle commitments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::collections::HashSet;
+use std::sync::Arc;
 
-use ark_ff::{AdditiveGroup, BigInt, FftField, Field, Fp, Zero};
+use ark_ff::{AdditiveGroup, BigInt, FftField, Field, Fp, PrimeField, Zero};
 use ark_ff::fields::{Fp64, MontBackend, MontConfig};
 use ark_ff::UniformRand;
+use ark_ff::BigInteger;
 use ark_poly::univariate::DensePolynomial;
-use ark_poly::DenseUVPolynomial;
+use ark_poly::{DenseUVPolynomial, Polynomial};
+
+use rs_merkle::{MerkleTree, MerkleProof, Hasher, algorithms::Sha256};
 
 #[derive(MontConfig)]
 #[modulus = "18446744069414584321"] // p = 2^64 - 2^32 + 1
@@ -23,7 +26,7 @@ pub struct FriParams {
     rate_parameter: u32,
     num_rounds: usize,
     k: Vec<u32>,
-    smooth_multiplicative_subgroup: Vec<HashSet<F>>,
+    smooth_multiplicative_subgroup: Vec<Vec<F>>,
     generator: Vec<F>,
     q: DensePolynomial<F>,
 }
@@ -38,10 +41,10 @@ impl FriParams {
         let two_to_the_eta: usize = 1 << eta;
 
 
-        // smooth_multiplicative_subgroup[i] is the subgroup used in the i-th round, implemented as a HashSet.
+        // smooth_multiplicative_subgroup[i] is the subgroup used in the i-th round, implemented as a Vec.
         // k[i] is the integer such that |smooth_multiplicative_subgroup[i]| = 2^{k[i]}.
         // generator[i] is a generator of smooth_multiplicative_subgroup[i].
-        let mut smooth_multiplicative_subgroup: Vec<HashSet<F>> = vec![];
+        let mut smooth_multiplicative_subgroup: Vec<Vec<F>> = vec![];
         let mut k: Vec<u32> = vec![];
         let mut generator: Vec<F> = vec![];
 
@@ -51,13 +54,13 @@ impl FriParams {
         for i in 0..=num_rounds {
             let k_i = n - (i as u32) * eta;
             let smooth_multiplicative_subgroup_i_order = 2_u64.pow(k_i);
-            let mut smooth_multiplicative_subgroup_i = HashSet::<F>::new();
+            let mut smooth_multiplicative_subgroup_i = Vec::<F>::new();
 
 
             // Add all the elements of L[i] to the set. We do this by starting with 1 and repeatedly multiplying by the generator.
             let mut smooth_multiplicative_subgroup_i_element = F::ONE;
             for j in 0..smooth_multiplicative_subgroup_i_order {
-                smooth_multiplicative_subgroup_i.insert(smooth_multiplicative_subgroup_i_element);
+                smooth_multiplicative_subgroup_i.push(smooth_multiplicative_subgroup_i_element);
                 smooth_multiplicative_subgroup_i_element = smooth_multiplicative_subgroup_i_element * generator_i;
             }
 
@@ -87,34 +90,33 @@ impl FriParams {
         }
     }
 }
-pub struct FriProverState<'a> {
-    params: &'a FriParams,
+pub struct FriProver {
+    params: Arc<FriParams>,
+    f: Vec<DensePolynomial<F>>,
+    f_merkle_tree: Vec<MerkleTree<Sha256>>,
 }
 
-pub struct FriProver<'a> {
-    state: FriProverState<'a>,
-    
-}
-
-impl<'a> FriProver<'a> {
-    fn new(params: &'a FriParams) -> FriProver<'a> {
-        FriProver {
-            state: FriProverState {
-                params
-            }
-        }
+impl FriProver {
+    fn new(params: Arc<FriParams>, polynomial: DensePolynomial<F>) -> FriProver {
+        let mut prover = FriProver {
+            params,
+            f: vec![polynomial],
+            f_merkle_tree: vec![]
+        };
+        prover.f_merkle_tree.push(prover.commit_polynomial(&prover.f[0], &prover.params.smooth_multiplicative_subgroup[0]));
+        prover
     }
 
     /// Perform a single polynomial folding step using the provided verifier challenge.
     /// Example: with an eta value of 1 and a verifier challenge of 3, running this on the polynomial
     /// 1 + 2x + 3x^2 + 4x^3 should return (1 + 3x) + 3*(2 + 4x) = 7 + 15x.
-    fn fold_polynomial(&self, polynomial: DensePolynomial<F>, verifier_challenge: F) -> DensePolynomial<F> {
+    fn fold_polynomial(&self, polynomial: &DensePolynomial<F>, verifier_challenge: F) -> DensePolynomial<F> {
 
         // Write the input polynomial in the form f(x) = f_0(x^{2^\eta}) + xf_1(x^{2^\eta}) + ... + x^{2^\eta - 1}f_{2^\eta - 1}(x^{2^\eta})
-        let mut sub_polynomials_coefficients: Vec<Vec<F>> = (0..self.state.params.two_to_the_eta).map(|_| vec![]).collect();
+        let mut sub_polynomials_coefficients: Vec<Vec<F>> = (0..self.params.two_to_the_eta).map(|_| vec![]).collect();
 
         for (i, coeff) in polynomial.coeffs().iter().enumerate() {
-            sub_polynomials_coefficients[i % self.state.params.two_to_the_eta].push(*coeff);
+            sub_polynomials_coefficients[i % self.params.two_to_the_eta].push(*coeff);
         }
         
         let sub_polynomials: Vec<DensePolynomial<F>> = sub_polynomials_coefficients.into_iter().map(
@@ -131,12 +133,40 @@ impl<'a> FriProver<'a> {
         folded_polynomial
     }
 
-    
+
+    /// Create a merkle tree and merkle proofs corresponding to the evaluations of a given polynomial
+    /// at every point in the given domain. depth is the depth of the resulting merkle tree, so
+    /// we must have 2^depth >= |domain|.
+    fn commit_polynomial(&self, polynomial: &DensePolynomial<F>, domain: &Vec<F>) -> MerkleTree<Sha256> {
+
+        // NOT ZERO KNOWLEDGE
+        // values is list where the entries are of the form hash(x, f(x)) for each x in the domain
+        let values = domain.into_iter().map(
+            |x| value_to_merkle_leaf(&polynomial, x)
+        ).collect::<Vec<[u8; 32]>>();
+        
+        let merkle_tree = MerkleTree::<Sha256>::from_leaves(&values);
+        merkle_tree
+    }
 
     fn execute_prover_round(round_index: usize, verifier_challenge: F){
         panic!("Not implemented");
     }
 }
+
+
+
+
+/// Deterministic function used to deterministically convert the value of a polynomial at a point
+/// to a format suitable for insertion in a Merkle tree.
+fn value_to_merkle_leaf(polynomial: &DensePolynomial<F>, point: &F ) -> [u8; 32] {
+    Sha256::hash(
+        vec![*point, polynomial.evaluate(point)].into_iter().flat_map(
+            |value| value.into_bigint().to_bytes_be()
+        ).collect::<Vec<u8>>().as_slice()
+    )
+}
+
 
 
 
@@ -154,25 +184,47 @@ fn main() {
     println!("params: {:?}", params);
 }
 
+#[cfg(test)]
 pub mod tests {
     use super::*;
 
-    #[test]
-    fn test_polynomial_folding(){
+
+    fn create_test_prover(polynomial: DensePolynomial<F>) -> (FriProver, Arc<FriParams>) {
         let eta = 1;
         let rate_parameter = 2;
         let degree_log_bound = 3;
-        let params = &FriParams::new(eta, rate_parameter, degree_log_bound);
-        let prover = FriProver {
-            state: FriProverState {
-                params
-            }
-        };
+        let params = Arc::new(FriParams::new(eta, rate_parameter, degree_log_bound));
+        
+        let prover = FriProver::new(params.clone(), polynomial.clone());
+        (prover, params)
+    }
+
+    #[test]
+    fn test_polynomial_folding(){
+        
         let polynomial = DensePolynomial::<F>::from_coefficients_vec(vec![F::from(1), F::from(2), F::from(3), F::from(4)]);
+        let (prover, _) = create_test_prover(polynomial);
         let verifier_challenge = F::from(3);
-        let result = prover.fold_polynomial(polynomial, verifier_challenge);
+        let result = prover.fold_polynomial(&prover.f[0], verifier_challenge);
         let expected = DensePolynomial::<F>::from_coefficients_vec(vec![F::from(7), F::from(15)]);
         println!("Result: {:?}", result);
+        //println!("params: {:?}", prover.params);
         assert_eq!(result, expected);
     }
+
+    #[test]
+    fn test_polynomial_commitment(){
+        let polynomial = DensePolynomial::<F>::from_coefficients_vec(vec![F::from(1), F::from(2), F::from(3), F::from(4)]);
+        let (prover, _) = create_test_prover(polynomial);
+        let merkle_tree = prover.commit_polynomial(&prover.f[0], &prover.params.smooth_multiplicative_subgroup[0]);
+        let proof = merkle_tree.proof(&[0]);
+        let root = merkle_tree.root().expect("Merkle tree did not return root");
+        let expected_leaf = value_to_merkle_leaf(&prover.f[0], &F::from(1));
+        let unexpected_leaf = value_to_merkle_leaf(&prover.f[0], &F::from(2));
+        assert!(proof.verify(root, &[0], &[expected_leaf], merkle_tree.leaves_len()));
+        assert!(! proof.verify(root, &[0], &[unexpected_leaf], merkle_tree.leaves_len()));
+    }
 }
+
+
+
